5b8145c9602762fbfffd49a7075f0a55036d42bf
PHJoon <phj9503@gmail.com>
2024-02-13 14:18:24 +0900

    feat: Message 디렉토리, 클래스 추가

---
+++ Message/Message.cpp
@@ -0,0 +1,93 @@
+#include "Message.hpp"
+
+Message::Message(std::string &origin) : m_origin(origin)
+{
+}
+
+Message::~Message()
+{
+}
+
+void Message::seperateOrigin()
+{
+    std::size_t start;
+    std::size_t pos;
+
+    bool is_prefix;
+    bool is_command;
+    bool is_params;
+
+    is_prefix = true;
+    is_command = false;
+    is_params = false;
+
+    pos = this->m_origin.find(' ');
+    while (pos != std::string::npos)
+    {
+        std::string tmp;
+        tmp = m_origin.substr(start, pos);
+
+        if (is_prefix)
+        {
+            is_prefix = false;
+            is_command = true;
+            if (tmp[0] == ':')
+            {
+                this->m_prefix = tmp;
+            }
+        }
+        else if (is_command)
+        {
+            is_command = false;
+            is_params = true;
+            this->m_command = tmp;
+        }
+        else if (is_params)
+        {
+            this->m_params.push_back(tmp);
+        }
+
+        pos = this->m_origin.find(' ', pos + 1);
+    }
+}
+
+/** Getter */
+
+const std::string Message::getOrigin() const
+{
+    return this->m_origin;
+}
+
+const std::string Message::getPrefix() const
+{
+    return this->m_prefix;
+}
+
+const std::string Message::getCommand() const
+{
+    return this->m_command;
+}
+
+const std::vector<std::string> Message::getParams() const
+{
+    return this->m_params;
+}
+
+/** Setter */
+
+void Message::setOrigin(std::string &origin)
+{
+    this->m_origin = origin;
+}
+void Message::setPrefix(std::string &prefix)
+{
+    this->m_prefix = prefix;
+}
+void Message::setCommand(std::string &command)
+{
+    this->m_command = command;
+}
+void Message::setParams(std::vector<std::string> &params)
+{
+    this->m_params = params;
+}
---
+++ Message/Message.hpp
@@ -0,0 +1,35 @@
+#ifndef MESSAGE_HPP
+#define MESSAGE_HPP
+
+#include <string>
+#include <vector>
+
+class Message
+{
+  private:
+    std::string m_origin;
+
+	std::string m_prefix;
+	std::string m_command;
+	std::vector<std::string> m_params;
+
+  public:
+    Message(std::string &origin);
+    ~Message();
+
+    void seperateOrigin();
+
+	/** Getter */
+    const std::string getOrigin() const;
+	const std::string getPrefix() const;
+	const std::string getCommand() const;
+	const std::vector<std::string> getParams() const;
+
+	/** Setter */
+    void setOrigin(std::string &origin);
+	void setPrefix(std::string &prefix);
+	void setCommand(std::string &command);
+	void setParams(std::vector<std::string> &params);
+};
+
+#endif
