7d72d9038fd2b3c2a4ebab89d818067cc26473fa
InryeolChoi <inchoi@cx2r1s1.42seoul.kr>
2024-02-13 14:43:05 +0900

    feat: Server/Client.cpp 추가

---
+++ Client/Client.cpp
@@ -0,0 +1,58 @@
+#include "Client.hpp"
+
+Client::Client()
+{
+}
+
+Client::Client(const Client &other)
+{
+    *this = other;
+}
+
+Client &Client::operator=(const Client &other)
+{
+    if (this != &other)
+    {
+        m_socket_fd = other.m_socket_fd;
+        m_nick = other.m_nick;
+        m_username = other.m_username;
+        m_cur_channel = other.m_cur_channel;
+        m_flag_connect = other.m_flag_connect;
+        m_is_op = other.m_is_op;
+    }
+    return *this;
+}
+
+Client::~Client()
+{
+}
+
+int Client::getsockfd()
+{
+    return m_socket_fd;
+}
+
+void Client::setRecvData(const char *data)
+{
+    std::string newstr(data);
+    m_recv_data += newstr;
+}
+
+std::string Client::getRecvData()
+{
+    return m_recv_data;
+}
+
+void Client::startListen(int serv_sock)
+{
+    struct sockaddr_in clnt_adr;
+    socklen_t clnt_len = sizeof(clnt_adr);
+
+    // accept()
+    m_socket_fd = accept(serv_sock, (struct sockaddr *)&clnt_adr, &clnt_len);
+    if (m_socket_fd < 0)
+        throw std::runtime_error("accept() error, client side");
+
+    // nonblock 처리
+    fcntl(m_socket_fd, F_SETFL, O_NONBLOCK);
+}
---
+++ Client/Client.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <iostream>
+#include <netinet/in.h>
+#include <stdexcept>
+#include <sys/event.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+class Client
+{
+  private:
+    int m_socket_fd;         // 소켓 fd
+    std::string m_recv_data; // 받은 데이터
+
+    std::string m_nick;        // 닉네임
+    std::string m_username;    // 유저네임
+    std::string m_cur_channel; // 현재 채널명
+    int m_flag_connect;        // 연결 여부
+    bool m_is_op;              // op(방장) 여부
+
+  public:
+    Client();
+    Client &operator=(const Client &other);
+    Client(const Client &other);
+    ~Client();
+
+    int getsockfd();
+    void setRecvData(const char *data);
+    std::string getRecvData();
+    void startListen(int serv_sock);
+};
