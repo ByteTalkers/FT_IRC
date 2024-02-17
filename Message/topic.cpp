b1d214905eaaf42804dcbe026349d1bc3763da4b
PHJoon <phj9503@gmail.com>
2024-02-18 02:41:59 +0900

    feat: topicExecute 멤버함수 추가

--- Message/topic.cpp
+++ Message/topic.cpp
@@ -3,14 +3,53 @@
 void Message::topicExecute(Server &server, Client &client)
 {
     // 파라미터 있다면,
-        // 파라미터 개수 확인
-            // 채널명 
-                // 해당 채널 있는지 확인 => 있으면 토픽 응답, 없으면 no such 채널
-            // 채널명 + 인자
-                // 해당 채널 있는지 확인 => 없으면 no such 채널
-                    // 인자 확인 후 
-                        // 채널 op인지 확인 후, 토픽 세팅 해주기
-
-    // 파라미터 없다면,
-        // needmoreparameter 에러
+    std::size_t params_count;
+    params_count = this->m_params.size();
+    if (params_count > 0)
+    {
+        std::string channel_name;
+        channel_name = this->m_params[0];
+        // 해당 채널 존재하는 지 확인
+        if (server.channelExist(channel_name) == true) 
+        {
+            // 파라미터 채널명만 들어온 경우 => 토픽 확인
+            Channel *channel;
+            channel = server.getChannels().at(channel_name);
+            if (params_count == 1) 
+            {
+                // 토픽 존재여부 확인
+                if (channel->getSetTopic() == true)
+                {
+                    Response::rplTopic_332(server.getName(), client.getNick(), channel_name, channel->getTopic());
+                }
+                else
+                {
+                    Response::rplNoTopic_331(server.getName(), client.getNick(), channel_name);
+                }
+            }
+            // 채널명 + 인자인 경우 => 토픽 세팅
+            else
+            {
+                // 채널 op인지 확인
+                if (channel->checkOp(&client) == true)
+                {
+                    channel->setTopic(this->m_params[1]);
+                }
+                else
+                {
+                    Response::errChanOPrivsNeeded_482(server.getName(), client.getNick(), channel_name);
+                }
+            }
+        }
+        // 없으면 no such 채널
+        else
+        {
+            Response::errNoSuchChannel_403(server.getName(), client.getNick(), channel_name);
+        }
+    }
+    // 파라미터 없다면, needmoreparameter 에러
+    else
+    {
+        Response::errNeedMoreParams_461(server.getName(), client.getNick(), this->m_command);
+    }
 }
