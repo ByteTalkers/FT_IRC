e8b1f8fb934adfdeb47523cd46b175e350894ed2
park hyung jun <55447614+wamma@users.noreply.github.com>
2024-02-19 23:03:19 +0900

    feat: join 처리 메서드를 위해 숫자를 문자열로 바꾸는 로직 추가

--- utils/utils.cpp
+++ utils/utils.cpp
@@ -3,9 +3,16 @@
 const std::string timeToString(time_t time)
 {
     char buffer[80];
-    struct tm* timeinfo;
+    struct tm *timeinfo;
     timeinfo = localtime(&time);
-    strftime(buffer, sizeof(buffer), "%H:%M:%S %b %d %Y" ,timeinfo);
+    strftime(buffer, sizeof(buffer), "%H:%M:%S %b %d %Y", timeinfo);
 
     return std::string(buffer);
+}
+
+std::string intToString(int value)
+{
+    std::ostringstream os;
+    os << value;
+    return os.str();
 }
