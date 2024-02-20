14c142993084ee2a2f8425c37d42a923de0b272c
PHJoon <phj9503@gmail.com>
2024-02-20 11:47:51 +0900

    feat: mode 파싱 완료

--- Message/mode.cpp
+++ Message/mode.cpp
@@ -1,145 +1,67 @@
 #include "Message.hpp"
 
-static bool checkModeModifier(const std::string &params, bool &is_plus);
-
-static void checkModeChar(const std::vector<std::string> &params, std::map<char, std::pair<bool, std::string> > &mode,
+static bool checkModeChar(const std::vector<std::string> &params, std::map<char, std::pair<bool, std::string> > &mode,
                           std::vector<char> &unknowns);
+static void modeI(Server &server, Client &client, Channel *channel, std::map<char, std::pair<bool, std::string> > &mode);
+static void modeT(Server &server, Client &client, Channel *channel, std::map<char, std::pair<bool, std::string> > &mode);
+static void modeK(Server &server, Client &client, Channel *channel, std::map<char, std::pair<bool, std::string> > &mode);
+static void modeO(Server &server, Client &client, Channel *channel, std::map<char, std::pair<bool, std::string> > &mode);
+static void modeL(Server &server, Client &client, Channel *channel, std::map<char, std::pair<bool, std::string> > &mode);
+static void modeUnknowns(Server &server, Client &client, std::vector<char> &unknowns);
 
 void Message::modeExecute(Server &server, Client &client, Command *cmd)
 {
-    client.addSendMsg(Response::rplUmodeIs_221(client.getUser(), client.getNick()));
-    client.setWriteTypes(MYSELF);
-    return;        
-    // 파라미터 있는지
-    if (cmd->getParamsCount() > 1)
+    // 파라미터 없음
+    if (cmd->getParamsCount() < 2)
     {
-        std::string channel_name;
-        channel_name = cmd->getParams()[0];
+        client.addSendMsg(Response::errNeedMoreParams_461(server.getName(), client.getNick(), cmd->getCommand()));
+        client.setWriteTypes(MYSELF);
+        return;
+    }
 
-        Channel *channel = server.findChannel(channel_name);
-        // 채널 체크
-        if (channel != NULL)
+    std::string channel_name;
+    channel_name = cmd->getParams()[0];
+    Channel *channel = server.findChannel(channel_name);
+    // 채널 체크
+    if (channel == NULL)
+    {
+        if (cmd->getParams()[0] == client.getNick())
         {
-            bool is_plus;
-            // +, - 체크
-            if (checkModeModifier(cmd->getParams()[1], is_plus) == true)
-            {
-                std::map<char, std::pair<bool, std::string> > mode;
-                std::vector<char> unknowns;
-                mode['i'] = std::make_pair(false, "");
-                mode['t'] = std::make_pair(false, "");
-                mode['k'] = std::make_pair(false, "");
-                mode['o'] = std::make_pair(false, "");
-                mode['l'] = std::make_pair(false, "");
-
-                // 존재하는 모드인지 확인 modechar확인
-                checkModeChar(cmd->getParams(), mode, unknowns);
-
-                if (mode.at('i').first == true)
-                {
-                    if (channel->checkOp(client) == true)
-                    {
-                        channel->setModeInvite(true);
-                        // mode +i 응답
-                    }
-                    else
-                    {
-                        client.addSendMsg(
-                            Response::errChanOPrivsNeeded_482(server.getName(), client.getNick(), channel_name));
-                    }
-                }
-                if (mode.at('t').first == true)
-                {
-                    if (channel->checkOp(client) == true)
-                    {
-                        channel->setModeTopic(true);
-                        // mode +t 응답
-                    }
-                    else
-                    {
-                        client.addSendMsg(
-                            Response::errChanOPrivsNeeded_482(server.getName(), client.getNick(), channel_name));
-                    }
-                }
-                if (mode.at('k').first == true)
-                {
-                    if (channel->checkOp(client) == true)
-                    {
-                        channel->setModeKey(true);
-                        channel->setKey(mode.at('k').second);
-                        // mode +k 응답
-                    }
-                    else
-                    {
-                        client.addSendMsg(
-                            Response::errChanOPrivsNeeded_482(server.getName(), client.getNick(), channel_name));
-                    }
-                }
-                if (mode.at('o').first == true)
-                {
-                    if (channel->checkOp(client) == true)
-                    {
-                        // 채널 오퍼레이터에 추가
-                        // mode +o 응답
-                    }
-                    else
-                    {
-                        client.addSendMsg(
-                            Response::errChanOPrivsNeeded_482(server.getName(), client.getNick(), channel_name));
-                    }
-                }
-                if (mode.at('l').first == true)
-                {
-                    if (channel->checkOp(client) == true)
-                    {
-                        channel->setModeLimit(true);
-                        channel->setLimitCount(std::atoi(mode.at('l').second.c_str()));
-                        // mode +l 응답
-                    }
-                    else
-                    {
-                        client.addSendMsg(
-                            Response::errChanOPrivsNeeded_482(server.getName(), client.getNick(), channel_name));
-                    }
-                }
-            }
-            // +, - 없으면 unknown 에러? 에러 종류 정하기
-            else
-            {
-                client.addSendMsg(
-                    Response::errUnknownMode_472(server.getName(), client.getNick(), cmd->getParams()[0]));
-            }
+            client.addSendMsg(Response::rplUmodeIs_221(client.getUser(), client.getNick()));
         }
         else
         {
-            if (cmd->getParams()[0] == client.getNick())
-            {
-                client.addSendMsg(Response::rplUmodeIs_221(client.getUser(), client.getNick()));
-            }
-            else
-            {
-                client.addSendMsg(Response::errNoSuchChannel_403(server.getName(), client.getNick(), channel_name));
-            }
+            client.addSendMsg(Response::errNoSuchChannel_403(server.getName(), client.getNick(), channel_name));
         }
+        client.setWriteTypes(MYSELF);
+        return;
     }
-    else
-    {
-        // 파라미터 없으면 needmore err
-        client.addSendMsg(Response::errNeedMoreParams_461(server.getName(), client.getNick(), cmd->getCommand()));
-    }
-}
 
-static bool checkModeModifier(const std::string &params, bool &is_plus)
-{
-    if (params[0] == '+' || params[0] == '-')
+
+    // 모드와 내용 key 담는 map
+    std::map<char, std::pair<bool, std::string> > mode;
+    mode['i'] = std::make_pair(false, "");
+    mode['t'] = std::make_pair(false, "");
+    mode['k'] = std::make_pair(false, "");
+    mode['o'] = std::make_pair(false, "");
+    mode['l'] = std::make_pair(false, "");
+
+    std::vector<char> unknowns;
+    // 존재하는 모드인지 확인 modechar확인
+    checkModeChar(cmd->getParams(), mode, unknowns);
+
+    if (!checkModeChar(cmd->getParams(), mode, unknowns))
     {
-        if (params[0] == '+')
-        {
-            is_plus = true;
-        }
-        return true;
+        // unknowns에 다 넣기
+        modeUnknowns(server, client, unknowns);
+        return;
     }
-    return false;
+    modeUnknowns(server, client, unknowns);
+    modeI();
+    modeT();
+    modeK();
+    modeO();
+    modeL();
 }
 
 static bool checkitkol(char c)
@@ -156,18 +78,35 @@ static bool checkitkol(char c)
     return false;
 }
 
-// check itkol
-static void checkModeChar(const std::vector<std::string> &params, std::map<char, std::pair<bool, std::string> > &mode,
+static bool checkModeChar(const std::vector<std::string> &params, std::map<char, std::pair<bool, std::string> > &mode,
                           std::vector<char> &unknowns)
 {
-    std::size_t idx;
-    idx = 1;
-    for (std::size_t i = 1; i < params[1].length(); i++)
+    // +, -로 시작 체크
+    if (params[1][0] != '+' && params[1][0] != '-')
     {
+        return false;
+    }
+
+    std::size_t idx = 1;
+    bool add = false;
+    for (std::size_t i = 0; i < params[1].length(); i++)
+    {
+        if (params[1][i] == '+' || params[1][i] == '-')
+        {
+            if (params[1][i] == '+')
+            {
+                add = true;
+            }
+            else
+            {
+                add = false;
+            }
+            continue;
+        }
         // itkol일 때,
         if (checkitkol(params[1][i]) == true)
         {
-            mode[params[1][i]].first = true;
+            mode[params[1][i]].first = add;
             if (params[1][i] == 'k' || params[1][i] == 'o' || params[1][i] == 'l')
             {
                 if (idx < params.size())
@@ -180,6 +119,120 @@ static void checkModeChar(const std::vector<std::string> &params, std::map<char,
         else
         {
             unknowns.push_back(params[1][i]);
+        }
+    }
+
+    // 남은 파라미터들은 unknowns로
+    while(idx < params.size())
+    {
+        for (std::size_t i = 0; i < params[idx].length(); i++)
+        {
+            unknowns.push_back(params[idx][i]);
+        }
+        idx++;
+    }
+
+    return true;
+}
+
+static void modeUnknowns(Server &server, Client &client, std::vector<char> &unknowns)
+{
+    std::vector<char>::iterator it;
+    for (it = unknowns.begin(); it < unknowns.end(); it++)
+    {
+        client.addSendMsg(
+            Response::errUnknownMode_472(server.getName(), client.getNick(), std::string(1, *it)));
+    }
+    client.setWriteTypes(MYSELF);
+}
+
+
+static void modeI(Server &server, Client &client, Channel *channel, std::map<char, std::pair<bool, std::string> > &mode)
+{
+    if (mode.at('i').first == true)
+    {
+        if (channel->checkOp(client) == true)
+        {
+            channel->setModeInvite(true);
+            // mode +i 응답
+        }
+        else
+        {
+            client.addSendMsg(
+                Response::errChanOPrivsNeeded_482(server.getName(), client.getNick(), channel->getName()));
+        }
+    }    
+}
+
+static void modeT(Server &server, Client &client, Channel *channel, std::map<char, std::pair<bool, std::string> > &mode)
+{
+    if (mode.at('t').first == true)
+    {
+        if (channel->checkOp(client) == true)
+        {
+            channel->setModeTopic(true);
+            // mode +t 응답
+        }
+        else
+        {
+            client.addSendMsg(
+                Response::errChanOPrivsNeeded_482(server.getName(), client.getNick(), channel_name));
+        }
+    }
+
+}
+
+static void modeK(Server &server, Client &client, Channel *channel, std::map<char, std::pair<bool, std::string> > &mode)
+{
+    if (mode.at('k').first == true)
+    {
+        if (channel->checkOp(client) == true)
+        {
+            channel->setModeKey(true);
+            channel->setKey(mode.at('k').second);
+            // mode +k 응답
+        }
+        else
+        {
+            client.addSendMsg(
+                Response::errChanOPrivsNeeded_482(server.getName(), client.getNick(), channel_name));
+        }
+    }
+
+}
+
+static void modeO(Server &server, Client &client, Channel *channel, std::map<char, std::pair<bool, std::string> > &mode)
+{
+    if (mode.at('o').first == true)
+    {
+        if (channel->checkOp(client) == true)
+        {
+            // 채널 오퍼레이터에 추가
+            // mode +o 응답
+        }
+        else
+        {
+            client.addSendMsg(
+                Response::errChanOPrivsNeeded_482(server.getName(), client.getNick(), channel_name));
+        }
+    }
+
+}
+
+static void modeL(Server &server, Client &client, Channel *channel, std::map<char, std::pair<bool, std::string> > &mode)
+{
+    if (mode.at('l').first == true)
+    {
+        if (channel->checkOp(client) == true)
+        {
+            channel->setModeLimit(true);
+            channel->setLimitCount(std::atoi(mode.at('l').second.c_str()));
+            // mode +l 응답
+        }
+        else
+        {
+            client.addSendMsg(
+                Response::errChanOPrivsNeeded_482(server.getName(), client.getNick(), channel_name));
         }
     }
 }
