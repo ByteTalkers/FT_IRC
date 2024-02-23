b1eb5f3bf84fb841c105272c60e0c7b8b008b4fa
PHJoon <phj9503@gmail.com>
2024-02-23 15:54:18 +0900

    comment: Message - ping 주석 추가

--- Message/ping.cpp
+++ Message/ping.cpp
@@ -1,5 +1,10 @@
 #include "Message.hpp"
 
+/**
+ * ping 실행부
+ * 파라미터 있으면 => 해당 파라미터를 담아서 pong 응답
+ * 파라미터 없으면 => ""를 담아서 응답
+*/
 void Message::pingExecute(Server &server, Client &client, Command *cmd)
 {
     if (cmd->getParams().empty())
