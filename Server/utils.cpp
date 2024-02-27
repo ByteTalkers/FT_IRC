3e2167c1c7110b10fd5caa2df6530933774316bb
InryeolChoi <inchoi@cx2r3s4.42seoul.kr>
2024-02-27 14:10:12 +0900

    feat: m_change_vec 사용 추가

--- Server/utils.cpp
+++ Server/utils.cpp
@@ -12,32 +12,36 @@ void Server::addReadEvent(int sockfd)
 {
     struct kevent read_event;
     EV_SET(&read_event, sockfd, EVFILT_READ, EV_ADD, 0, 0, NULL);
-    if (kevent(m_kqueue, &read_event, 1, NULL, 0, NULL) == -1)
-        throw std::runtime_error("kevent() of addReadEvent was wrong..");
+    m_change_vec.push_back(read_event);
+    // if (kevent(m_kqueue, &read_event, 1, NULL, 0, NULL) == -1)
+    //     throw std::runtime_error("kevent() of addReadEvent was wrong..");
 }
 
 void Server::addWriteEvent(int sockfd)
 {
     struct kevent write_event;
     EV_SET(&write_event, sockfd, EVFILT_WRITE, EV_ADD, 0, 0, NULL);
-    if (kevent(m_kqueue, &write_event, 1, NULL, 0, NULL) == -1)
-        throw std::runtime_error("kevent() of addWriteEvent was wrong..");
+    m_change_vec.push_back(write_event);
+    // if (kevent(m_kqueue, &write_event, 1, NULL, 0, NULL) == -1)
+    //     throw std::runtime_error("kevent() of addWriteEvent was wrong..");
 }
 
 void Server::disableWriteEvent(int sockfd)
 {
     struct kevent write_event;
     EV_SET(&write_event, sockfd, EVFILT_WRITE, EV_DISABLE, 0, 0, NULL);
-    if (kevent(m_kqueue, &write_event, 1, NULL, 0, NULL) == -1)
-        throw std::runtime_error("kevent() of disableWriteEvent was wrong..");
+    m_change_vec.push_back(write_event);
+    // if (kevent(m_kqueue, &write_event, 1, NULL, 0, NULL) == -1)
+    //     throw std::runtime_error("kevent() of disableWriteEvent was wrong..");
 }
 
 void Server::enableWriteEvent(int sockfd)
 {
     struct kevent write_event;
     EV_SET(&write_event, sockfd, EVFILT_WRITE, EV_ENABLE, 0, 0, NULL);
-    if (kevent(m_kqueue, &write_event, 1, NULL, 0, NULL) == -1)
-        throw std::runtime_error("kevent() of enableWriteEvent was wrong..");
+    m_change_vec.push_back(write_event);
+    // if (kevent(m_kqueue, &write_event, 1, NULL, 0, NULL) == -1)
+    //     throw std::runtime_error("kevent() of enableWriteEvent was wrong..");
 }
 
 void Server::enableMultipleWrite(Client &clnt)
